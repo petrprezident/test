3df43f8ec7e52291363dacaa0b0705233b49ca61
petrprezident <155640176+petrprezident@users.noreply.github.com>
2024-01-04 13:36:54 +0100

    Add files via upload

---
+++ librenms/rootfs/etc/cont-init.d/02-fix-perms.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/with-contenv sh
+# shellcheck shell=sh
+set -e
+
+echo "Fixing perms..."
+mkdir -p /data \
+  /var/run/nginx \
+  /var/run/php-fpm
+chown librenms:librenms \
+  /data \
+  "${LIBRENMS_PATH}" \
+  "${LIBRENMS_PATH}/.env"
+chown -R librenms:librenms \
+  /home/librenms \
+  /tpls \
+  /var/lib/nginx \
+  /var/log/nginx \
+  /var/log/php81 \
+  /var/run/nginx \
+  /var/run/php-fpm
